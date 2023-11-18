f752d243beac33997f2251074d44aada432142c8
Abrar <amahi2001@gmail.com>
2023-11-18 00:51:40 -0500

    Add SQLAlchemy configuration and Dog model, update index.html to display dog information

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route("/hello")
+# adding configuration for using a sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost/intern'
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
+
+
+class Dog(db.Model):
+    __tablename__ = 'dogs'
+    dog_tag = db.Column(db.Integer, primary_key=True)
+    d_name = db.Column(db.String(20))
+    d_age = db.Column(db.String(20))
+    d_type = db.Column(db.String(20))
+
+
+@app.route("/")
 def hello_world():
-    return render_template('index.html')
+    dogs = Dog.query.all()
+    
+    return render_template('index.html', dogs=dogs)
